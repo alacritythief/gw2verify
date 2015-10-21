585ec95a1460aed1c4ff531a1d5f35b0bcacc7c6
Andy Wong <awong@cogolabs.com>
2015-10-21 16:02:54 -0400

    works

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from verify import *
 
 app = Flask(__name__)
@@ -6,14 +6,17 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/<api_key>")
 def index(api_key=None):
-
     if api_key is not None:
         verify = define_person(api_key)
     else:
         verify = None
-
     return render_template('index.html', context=verify)
 
+@app.route("/verify", methods=['POST'])
+def verify_form():
+    api_key = request.form['api_key']
+    return redirect('/' + api_key)
+
 if __name__ == "__main__":
-    app.debug = True
+    app.debug = False
     app.run()
